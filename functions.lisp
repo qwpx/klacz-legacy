06c92c717b5942906ae387395b09084c4d5f4fc7
dodek <devnull@localhost>
2011-02-07 00:52:01 +0100

    ping i m8b

--- functions.lisp
+++ functions.lisp
@@ -83,3 +83,38 @@
 
 (def bot-function :more (irc-reactor message line)
   (call-reactor irc-reactor :more message))
+
+(defparameter *m8b-answers*
+  '("As I see it, yes"
+    "It is certain"
+    "It is decidedly so"
+    "Most likely"
+    "Outlook good"
+    "Signs point to yes"
+    "Without a doubt"
+    "Yes"
+    "Yes - definitely"
+    "You may rely on it"
+    "Reply hazy, try again"
+    "Ask again later"
+    "Better not tell you now"
+    "Cannot predict now"
+    "Concentrate and ask again"
+    "Don't count on it"
+    "My reply is no"
+    "My sources say no"
+    "Outlook not so good"
+    "Very doubtful"
+    "You have AIDS anyway"))
+
+(def bot-function :8b (irc-reactor message line)
+  "Randomly answers to the given question."
+  (call-reactor irc-reactor :reply-to message 
+		(nth (random (length *m8b-answers*))
+		     *m8b-answers*)))
+
+(def bot-function :ping (irc-reactor message line)
+  "Pings back the author of a message."
+  (call-reactor irc-reactor :reply-to message 
+		(format nil "~A: pong" (source message))))
+
